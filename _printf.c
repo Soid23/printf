cca51c532f775f14dff2f501e75d84b935ab0711
rabs23 <ruthrabs22@gmail.com>
2023-10-16 21:24:53 -0700

    updated'

--- _printf.c
+++ _printf.c
@@ -5,24 +5,22 @@
  * @format: the identifier
  * Return: length
  */
-
 int _printf(const char *format, ...)
 {
-	select_match match[] = {
+	check mate[] = {
 		{"%s", print_string}, {"%c", print_char},
-		{"%%", print_per}, {"%d", print_din},
+		{"%%", print_per}, {"%d", print_decimal},
 		{"%i", print_int}, {"%b", print_bin},
 		{"%u", print_unsigned_int}, {"%o", print_oct},
 		{"%x", print_hexa}, {"%X", print_hex},
 		{"%S", print_str}, {"%p", print_pointer},
 		{"%r", print_rev}, {"%R", print_rot}
 	};
-	va_list ap;
+	va_list args;
 	int a = 0, b;
 	int len = 0;
 
-	va_start(ap, format);
-
+	va_start(args, format);
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 	while (format[a] != '\0')
@@ -30,9 +28,9 @@ int _printf(const char *format, ...)
 		b = 13;
 		while (b >= 0)
 		{
-			if (match[b].sym[0] == format[a] && match[b].sym[1] == format[a + 1])
+			if (mate[b].sym[0] == format[a] && mate[b].sym[1] == format[a + 1])
 			{
-				len = len + match[b].func(ap);
+				len = len + mate[b].f(args);
 				a = a + 2;
 				break;
 			}
@@ -47,6 +45,6 @@ int _printf(const char *format, ...)
 		else if (format[a] == '\0')
 			break;
 	}
-	va_end(ap);
+	va_end(args);
 	return (len);
 }
