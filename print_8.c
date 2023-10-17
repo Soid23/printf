40a2ddbcc9e51839611c951b0dcf9939f1b5f4ab
rabs23 <ruthrabs22@gmail.com>
2023-10-16 22:14:14 -0700

    updated

--- print_8.c
+++ print_8.c
@@ -1,10 +1,37 @@
 #include "main.h"
 
+/**
+ * print_8 - a function that converts digits to octal
+ * @val: arguments passed to the function
+ * Return: count
+ */
+
 int print_8(va_list val)
 {
 	int b;
-	counter = 0;
 
-	unsigned int num = va_arg(val, unsigned int);
+	count = 0;
+	int *arr;
 
-	while (num / 8 != 0)
+	unsigned int new = clip;
+	unsigned int clip = va_arg(val, unsigned int);
+
+	while (clip / 8 != 0)
+	{
+		clip = clip / 8;
+		count++;
+	}
+	count++;
+
+	arr = malloc(sizeof(int) * count);
+	if (arr == NULL)
+		return (NULL);
+	for (b = 0; b < counter; b++)
+	{
+		arr[b] = new % 8;
+		new = new / 8;
+	}
+	for (b = count - 1; b >= 0; b++)
+		_putchar(arr[b] + '0');
+	free(arr);
+	return (count);
