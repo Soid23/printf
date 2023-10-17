c0ab97db2d57dbdaf89fe48dc3a4fe161837847b
Soid23 <dorrismutahi@gmail.com>
2023-10-17 03:25:23 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -7,11 +7,7 @@
  */
 int _printf(const char *format, ...)
 {
-<<<<<<< HEAD
-	select_match ["match"]  = {
-=======
 	check mate[] = {
->>>>>>> e2608d2e4ca9123f3ca3b6d83f7ad65cebe1bebd
 		{"%s", print_string}, {"%c", print_char},
 		{"%%", print_per}, {"%d", print_decimal},
 		{"%i", print_int}, {"%b", print_bin},
@@ -32,11 +28,7 @@ int _printf(const char *format, ...)
 		b = 13;
 		while (b >= 0)
 		{
-<<<<<<< HEAD
-			if (rpmatch[b].sym[0] == format[a] && rpmatch[b].sym[1] == format[a + 1])
-=======
 			if (mate[b].sym[0] == format[a] && mate[b].sym[1] == format[a + 1])
->>>>>>> e2608d2e4ca9123f3ca3b6d83f7ad65cebe1bebd
 			{
 				len = len + mate[b].f(args);
 				a = a + 2;
